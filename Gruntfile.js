22427ba54ba22174a2da55dd60baecc5adb2025b
Rolandas Barysas <r.b@riseup.net>
2014-06-06 16:14:42 +0300

    added compass/watch/shell to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		
 		nodewebkit: {
 			options: {
 				build_dir: "./build",
@@ -10,11 +11,39 @@ module.exports = function(grunt) {
 				linux64: true
 			},
 			src: ['./src/**/*', './package.json']
+		},
+
+		compass: {
+			dist: {
+				options: {
+					sassDir: 'src/sass',
+					cssDir: 'src',
+					fontsDir: 'src/fonts',
+					specify: 'src/sass/main.sass'
+				}
+			}
+		},
+
+		watch: {
+			scripts: {
+				files: ['src/sass/**/*.sass'],
+				tasks: ['compass']
+			}
+		},
+
+		shell: {
+			start: {
+				command: '"build/cache/linux64/0.9.2/nw" .'
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-node-webkit-builder');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-shell');
 
-	grunt.registerTask('default', []);
+	grunt.registerTask('default', ['compass', 'watch']);
 	grunt.registerTask('build', 'nodewebkit');
+	grunt.registerTask('start', 'shell');
 }
