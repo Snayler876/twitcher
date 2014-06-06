c93ff188d09605e1d0a9faed9451ffbff694dbeb
Rolandas Barysas <r.b@riseup.net>
2014-06-06 15:24:27 +0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		nodewebkit: {
+			options: {
+				build_dir: "./build",
+				win: false,
+				mac: false,
+				linux32: false,
+				linux64: true
+			},
+			src: ['./src/**/*', './package.json']
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-node-webkit-builder');
+
+	grunt.registerTask('default', []);
+	grunt.registerTask('build', 'nodewebkit');
+}
